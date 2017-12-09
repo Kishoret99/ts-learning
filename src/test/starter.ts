fe51ec1393192bfe8adc120cb538eebb93f160a6
SaiKishoret99 <saikishoret99@gmail.com>
2017-12-09 17:40:44 +0530

    first commit

---
+++ src/test/starter.ts
@@ -0,0 +1,10 @@
+describe('some feature', () => {
+    it('should pass', () => {
+        'foo'.should.not.equal('bar');
+    });
+    it('should error', () => {
+        (() => {
+            throw new Error();
+        }).should.throw();
+    });
+});
